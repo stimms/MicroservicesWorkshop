c5a297452e7cfe8039231d649a4d9627fb69adc9
Simon Timms <stimms@gmail.com>
2019-10-20 16:43:32 -0600

    Adding search for javascript

---
+++ javascript/database.js
@@ -0,0 +1,34 @@
+var rooms = [
+    {
+        id: "b3eeac1f-a473-41b3-a648-e4dba0b2d96e",
+        name:"Garden view",
+        bookings: []
+    },
+    {
+        id: "aabbe1f-a473-41b3-a648-e4dba0b2d96e",
+        name: "Sea View",
+        bookings: []
+    },
+    {
+        id: "128bec1f-a473-41b3-a648-e4dba0b2d96e",
+        name: "Broom closet",
+        bookings: []
+    }
+];
+
+function addBooking(roomId, booking){
+    rooms.find(x=>x.id == roomId).bookings.push(booking);
+};
+
+function cancelBooking(bookingId){
+    let room = rooms.find(x=>x.bookings.find(bookingId).length > 0);
+    room.bookings = room.bookings.filter(x=>x.id !== bookingId);
+}
+
+function findEmptyRooms(startDate, endDate){
+    return rooms.filter(x=>(x.bookings.filter(y=> 
+        (endDate >= y.startDate && endDate < y.endDate) || 
+        (startDate >= y.StartDate && startDate <= y.EndDate)).length === 0));
+}
+
+module.exports = { rooms, addBooking, cancelBooking, findEmptyRooms};
